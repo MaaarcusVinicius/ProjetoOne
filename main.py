2f3495c6c2f927718f65c83aa61bf0314b51b17b
Marcus Vinicius <maaarcus.vinicius@gmail.com>
2024-08-02 13:10:18 -0300

    Primeiro Commit

---
+++ main.py
@@ -0,0 +1,18 @@
+print("jornada de dados")
+
+#Variáveis
+idade = 31
+nome = "Marcus Vinicius"
+
+# Print
+print(idade)
+
+#Soma
+nova_idade = idade+1
+print(nova_idade)
+
+#Listas
+lista = [1,2,3,4,5]
+
+#Métodos
+lista_invertida = lista.reverse()
